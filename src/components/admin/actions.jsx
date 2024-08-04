106ad11bce37f94d49fe13d527381f79ce2128cd
Luis Klembt <luis.klembt@gmx.de>
2024-08-04 11:00:01 +0200

    fire base hosting customisation, first build, begin admin add Item

--- src/components/admin/actions.jsx
+++ src/components/admin/actions.jsx
@@ -14,8 +14,8 @@ class Actions extends Component {
                     <li className="list-group-item">
                         <div className="input-group flex-nowrap">
                         <input id="add-input" type="text" className="form-control" placeholder="Code"/>
-                        <input type="text" placeholder='Name'/>
-                        <input type="number"  placeholder='Preis'/>
+                        <input type="text" className="form-control" placeholder='Name'/>
+                        <input type="number" className="form-control" placeholder='Preis'/>
                         <button id="add-button" type="button" className="btn btn-primary">Produkt hinzufügen</button>
                         </div>
                     </li>
